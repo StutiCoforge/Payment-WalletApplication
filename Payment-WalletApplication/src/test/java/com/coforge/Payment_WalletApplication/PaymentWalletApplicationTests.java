dac6b25e8cb7bd0b51ca2ae6643c6cde580f05e1
StutiCoforge <Stuti.Kesarwani@coforge.com>
2026-03-24 14:44:06 +0530

    Create Project Structure

---
+++ Payment-WalletApplication/src/test/java/com/coforge/Payment_WalletApplication/PaymentWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coforge.Payment_WalletApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
