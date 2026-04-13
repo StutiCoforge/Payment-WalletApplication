281e533c2534ba167750f5a20b37bf03db8a59f5
gaurav-kuriyal <Gaurav.Kuriyal@coforge.com>
2026-04-13 15:19:25 +0530

    beneficiary bug fixed

--- Payment-WalletFrontend/springreact/src/services/beneficiaryService.js
+++ Payment-WalletFrontend/springreact/src/services/beneficiaryService.js
@@ -6,8 +6,7 @@ export const addBeneficiary = (data) => api.post('/auth/beneficiary', data);
 export const deleteBeneficiary = (id) => api.delete(`/auth/beneficiary/${id}`);
 
 export const sendMoneyApi = (mobileNumber, amount) => {
-  return api.post(`/auth/beneficiary/mobile/sendMoney/${mobileNumber}`, {
-    amount,
+  return api.post(`/auth/beneficiary/mobile/sendMoney/${mobileNumber}?amount=${amount}`, {
     mobileNumber
   });
 };
